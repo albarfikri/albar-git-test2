3c16c4d0a95784e80313ac82d37a7f5cf77d3954
albarfikri4 <fikrialbar4@gmail.com>
2021-05-18 12:26:47 +0700

    first commit

---
+++ app/src/main/java/com/cap0323/test/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cap0323.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
